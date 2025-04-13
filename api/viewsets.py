989dfb452cd9f951465543b09afd97d5760ed290
Vitali Kaliutchuk <vitalikaliutchyk@gmail.com>
2025-04-13 22:46:20 +0300

    Add endpoints

---
+++ api/viewsets.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets, permissions
+from blog.models import Post, Comment
+from api.serializers import PostSerializer, CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
