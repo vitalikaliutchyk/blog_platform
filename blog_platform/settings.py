d105385f271854f34078fa82390a2b29dafa17b5
Vitali Kaliutchuk <vitalikaliutchyk@gmail.com>
2025-04-24 21:40:02 +0300

    Modified settings static

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -161,8 +161,11 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
 
 # Настройки для статических файлов
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Для production
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Указываем нашу папку static
+]
 
 VIEWS_COUNTER_REDIS_KEY = "post_views:{post_id}"
 
@@ -181,12 +184,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
